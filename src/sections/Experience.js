5fd53fa8707fdcdf32ed56f8bd3e7522fba5d37b
Daniel Dominguez <ddominguez@metropolitan-touring.com>
2024-09-01 17:22:25 -0500

    Mejoras de Experiencia terminada

--- src/sections/Experience.js
+++ src/sections/Experience.js
@@ -1,41 +1,57 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const Experience = () => {
+    const [expandedItems, setExpandedItems] = useState([true, true, true]);
+
+    const handleToggle = (index) => {
+        const newExpandedItems = [...expandedItems];
+        newExpandedItems[index] = !newExpandedItems[index];
+        setExpandedItems(newExpandedItems);
+    };
+
     return (
         <section id="experience" className="experience-section">
             <h2>Experience</h2>
             <div className="timeline-container">
                 <div className="timeline">
+                    {/* Primer trabajo */}
                     <div className="timeline-item">
+                        <div className="circle" onClick={() => handleToggle(0)}></div>
                         <div className="timeline-date">2022 - Present</div>
-                        <div className="timeline-content">
-                            <h3>Analista de Programacion</h3>
+                        <div className={`timeline-content ${expandedItems[0] ? 'expanded' : 'collapsed'}`}>
+                            <h3>Analista de Programación</h3>
                             <h4>Metropolitan Touring, Quito, Ecuador</h4>
                             <p>
                                 Responsable de analizar, diseñar y desarrollar soluciones de software.
-                                Trabajando con tecnologias como Git, atencion al detalle y otros.
+                                Trabajando con tecnologías como Git, atención al detalle y otras 8 habilidades destacadas.
                             </p>
                         </div>
                     </div>
+
+                    {/* Segundo trabajo */}
                     <div className="timeline-item">
+                        <div className="circle" onClick={() => handleToggle(1)}></div>
                         <div className="timeline-date">2021 - 2022</div>
-                        <div className="timeline-content">
-                            <h3>Ingeniero de Software Junior</h3>
+                        <div className={`timeline-content ${expandedItems[1] ? 'expanded' : 'collapsed'}`}>
+                            <h3>Ingeniero de Software Júnior</h3>
                             <h4>Metropolitan Touring, En remoto</h4>
                             <p>
-                                Responsable de analizar, diseñar y desarrollar soluciones de software.
-                                Trabajando con tecnologias como Git, atencion al detalle y otros.
+                                Desarrollé aplicaciones usando programación en .NET y Entity Framework.
+                                Mejora de procesos de desarrollo y colaboración en un entorno ágil.
                             </p>
                         </div>
                     </div>
+
+                    {/* Tercer trabajo */}
                     <div className="timeline-item">
+                        <div className="circle" onClick={() => handleToggle(2)}></div>
                         <div className="timeline-date">2021</div>
-                        <div className="timeline-content">
+                        <div className={`timeline-content ${expandedItems[2] ? 'expanded' : 'collapsed'}`}>
                             <h3>Pasante IT</h3>
                             <h4>Metropolitan Touring, En remoto</h4>
                             <p>
-                                Responsable de analizar, diseñar y desarrollar soluciones de software.
-                                Trabajando con tecnologias como Git, atencion al detalle y otros.
+                                Participación en proyectos de soporte técnico y desarrollo de software durante el período de prácticas.
+                                Aprendí habilidades clave de programación y gestión de bases de datos.
                             </p>
                         </div>
                     </div>
--- src/sections/Home.js
+++ src/sections/Home.js
@@ -1,25 +1,31 @@
 import {scrollToSection} from "../utils/scrollTo";
 
 const Home = () => {
-    return (
-        <section id="home">
-            <div className="shapes">
-                <div className="cube"></div>
-                <div className="sphere"></div>
-            </div>
+    return (<section id="home" className="home-section">
+            <div className="home-content">
+                <div className="home-text">
+                    <div className="shapes">
+                        <div className="cube"></div>
+                        <div className="sphere"></div>
+                    </div>
 
-            <h1>Daniel WorkLab</h1>
-            <p className="subtitle">SOFTWARE ENGINEER, FULL STACK DEVELOPER</p>
-            <div className="scroll-icon" onClick={() => scrollToSection('expertise')}>
-                <span className="mouse">
-                    <span className="mouse-in"></span>
-                </span>
-                <span className="arrow"></span>
-            </div>
+                    <h1>Daniel WorkLab</h1>
+                    <p className="subtitle">SOFTWARE ENGINEER, FULL STACK DEVELOPER</p>
+                    <div className="scroll-icon" onClick={() => scrollToSection('expertise')}>
+                    <span className="mouse">
+                        <span className="mouse-in"></span>
+                    </span>
+                        <span className="arrow"></span>
+                    </div>
+                    <div className="cube"></div>
+                </div>
 
-            <div className="cube"></div>
-        </section>
-    );
+                <div className="home-image">
+                    <img src="ruta/foto.jpg" alt="Daniel" className="profile-image"/>
+                </div>
+
+            </div>
+        </section>);
 }
 
 export default Home;
