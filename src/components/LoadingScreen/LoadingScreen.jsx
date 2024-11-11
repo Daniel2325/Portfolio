25deb52898cfc56a0b72cd101de3e77569eecc36
Daniel Dominguez <ddominguez@metropolitan-touring.com>
2024-11-10 23:49:12 -0500

    Ajustes en escritura y redaccion

--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -1,11 +1,16 @@
 import './LoadingScreen.css';
 import devops from "../../assets/images/devops.png";
+
 const LoadingScreen = () => {
     return (
-        <div className="loading-screen">
-            <img src={devops} alt="Cargando..." className="loading-gif"/>
-            <p className="loading-text">Preparando tu experiencia DevOps...</p>
-        </div>
+        <>
+            <div className="loading-screen">
+                {/*<img src={devops} alt="Cargando..." className="loading-gif"/>*/}
+                <p className="loading-text">Preparando tu experiencia DevOps...</p>
+
+            </div>
+
+        </>
     );
 };
 
--- src/sections/Experience.js
+++ src/sections/Experience.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 
 const Experience = () => {
@@ -12,23 +12,40 @@ const Experience = () => {
 
     return (
         <section id="experience" className="experience-section">
-            <h2>Experience</h2>
+            <h2>Experiencia</h2>
             <div className="timeline-container">
                 <div className="timeline">
                     {/* Primer trabajo */}
                     <div className="timeline-item">
-                        <div className="timeline-date"onClick={() => handleToggle(0)} >2022 - Present</div>
+                        <div className="timeline-date" onClick={() => handleToggle(0)}>2022 - Actualidad</div>
                         <div className={`timeline-content ${expandedItems[0] ? 'expanded' : 'collapsed'}`}>
                             <h3>Analista Programador</h3>
                             <h4>Metropolitan Touring, Quito, Ecuador</h4>
                             <p>
-                                Responsable de liderar mejoras continuas en los sistemas de la empresa, específicamente en Salesforce y en la integración de sistemas hoteleros y de operaciones utilizando patrones de arquitectura para optimizar procesos.
+                                Como Analista Programador, lidero mejoras continuas en los sistemas de la empresa, con
+                                un enfoque específico en Salesforce e integración de sistemas hoteleros y operativos. Mi
+                                rol incluye el uso de patrones de arquitectura y técnicas avanzadas para optimizar
+                                procesos y asegurar la continuidad de las operaciones
                             </p>
                             <ul>
-                                <li>Optimización de flujos en Salesforce: Desarrollé y mejoré funcionalidades mediante Apex, triggers, LWC y flows, logrando un aumento significativo en la eficiencia de los procesos comerciales y operativos.</li>
-                                <li>Resolución de errores críticos: Brindé soporte de nivel 2 para garantizar la continuidad de los procesos comerciales, reduciendo tiempos de inactividad en un 30%.</li>
-                                <li>Integración de sistemas: Refactoricé las integraciones entre Salesforce y sistemas como Opera y XTS, lo que resultó en una reducción del 90% en errores de conectividad y problemas de timeout.</li>
-                                <li>Automatización de despliegues: Implementé DevOps Center en Salesforce, lo que permitió una automatización completa del ciclo de despliegue, mejorando el control sobre los entornos.</li>
+                                <li> Optimización de Flujos en Salesforce: Desarrollé y mejoré funcionalidades mediante
+                                    Apex, Triggers, LWC y Flows, logrando un aumento significativo en la eficiencia de
+                                    los procesos comerciales y operativos.
+                                </li>
+                                <li>Resolución de Errores Críticos: Proporcioné soporte de nivel 2 para garantizar la
+                                    continuidad de los procesos comerciales, logrando una reducción del 30% en tiempos
+                                    de inactividad mediante una atención eficaz a incidentes y una resolución rápida de
+                                    problemas.
+                                </li>
+                                <li>Integración de Sistemas: Refactoricé las integraciones entre Salesforce y sistemas
+                                    críticos como Opera y XTS, obteniendo una reducción del 90% en errores de
+                                    conectividad y problemas de timeout en ubicaciones con poca cobertura de red.
+                                </li>
+                                <li>Automatización de Despliegues: Implementé DevOps Center en Salesforce, automatizando
+                                    el ciclo completo de despliegue y mejorando el control sobre los entornos. Esta
+                                    automatización ha permitido una gestión más ágil y una entrega continua de
+                                    actualizaciones de alta calidad.
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -37,15 +54,30 @@ const Experience = () => {
                     <div className="timeline-item">
                         <div className="timeline-date" onClick={() => handleToggle(1)}>2021 - 2022</div>
                         <div className={`timeline-content ${expandedItems[1] ? 'expanded' : 'collapsed'}`}>
-                            <h3>Programador Júnior</h3>
+                            <h3>Programador</h3>
                             <h4>Metropolitan Touring, En remoto</h4>
                             <p>
-                                Participación en el desarrollo del sistema CORE: Colaboré en la construcción de un nuevo sistema de operaciones, reemplazando una solución obsoleta, lo que mejoró significativamente los tiempos de respuesta y rendimiento de los procesos operativos.
+                                Como Programador, participé activamente en el desarrollo de un sistema CORE para
+                                operaciones, reemplazando una solución obsoleta. Este proyecto mejoró considerablemente
+                                los tiempos de respuesta y la eficiencia de los procesos operativos, contribuyendo a una
+                                infraestructura tecnológica más moderna y escalable.
                             </p>
                             <ul>
-                                <li>Automatización de desarrollo: Implementé pipelines de CI/CD con Azure DevOps, logrando una reducción del 99% en los tiempos de despliegue y mejorando la calidad del código mediante SonarCloud.</li>
-                                <li>Microservicios y escalabilidad: Diseñé e implementé microservicios en .NET, aumentando la escalabilidad y reduciendo los costos de infraestructura al integrar Docker en los flujos de CI/CD.</li>
-                                <li>El frontend fue desarrollado utilizando React y TypeScript, lo que permitió una experiencia de usuario más rica y mantenible.<span className="highlight">15%</span>.</li>
+                                <li>Automatización del Desarrollo: Implementé pipelines de CI/CD con Azure DevOps,
+                                    logrando una reducción del 99% en los tiempos de despliegue. Además, aseguré una
+                                    mejora continua en la calidad del código mediante análisis con SonarCloud, lo que
+                                    permitió identificar y resolver problemas de forma temprana.
+                                </li>
+                                <li>Microservicios y Escalabilidad: Diseñé e implementé microservicios en .NET,
+                                    mejorando la escalabilidad del sistema y optimizando los costos de infraestructura.
+                                    Integré Docker en los flujos de CI/CD, facilitando la administración de entornos y
+                                    la eficiencia en los despliegues.
+                                </li>
+                                <li>Desarrollo Frontend en React y TypeScript: Colaboré en la creación de una interfaz
+                                    de usuario rica y mantenible con React y TypeScript, lo cual incrementó la
+                                    satisfacción del usuario en un 15%, ofreciendo una experiencia visual y funcional
+                                    mejorada..<span className="highlight">15%</span>.
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -57,11 +89,21 @@ const Experience = () => {
                             <h3>Pasante IT</h3>
                             <h4>Metropolitan Touring, En remoto</h4>
                             <p>
-                                Apoyo en la mejora de procesos internos y en el control de la calidad del código desarrollado en el área de IT.
+                                Durante mi pasantía en el área de IT, apoyé la mejora de procesos internos y contribuí
+                                en el control de calidad del código, logrando optimizaciones clave en los flujos de
+                                trabajo y en la consistencia del desarrollo.
                             </p>
                             <ul>
-                                <li>Optimización de procesos de contratación IT: Colaboración en la mejora del flujo de contratación de proveedores IT, asegurando un control más eficiente del código desarrollado.</li>
-                                <li>Control de calidad de código: Desarrollo de reportes y herramientas para asegurar la calidad y cumplimiento de los estándares de desarrollo.</li>
+                                <li>Optimización del Proceso de Contratación IT: Colaboré en la optimización del flujo
+                                    de contratación de proveedores IT, implementando medidas que aseguraron un control
+                                    más eficiente y ágil, mejorando la eficiencia en la selección y contratación de
+                                    servicios tecnológicos.
+                                </li>
+                                <li>Control de Calidad de Código: Desarrollé reportes y herramientas específicas para
+                                    monitorear y asegurar el cumplimiento de estándares de calidad en el código
+                                    desarrollado, contribuyendo a una base de código más robusta y alineada con las
+                                    mejores prácticas.
+                                </li>
                             </ul>
                         </div>
                     </div>
